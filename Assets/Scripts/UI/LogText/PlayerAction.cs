a8c7962ac8cbb024c4614f465a1a242734436a7a
Christer Kaivo-oja <christer@fumbbl.com>
2019-11-26 23:35:21 +0100

    Remove unused descriptions.

--- Assets/Scripts/UI/LogText/PlayerAction.cs
+++ Assets/Scripts/UI/LogText/PlayerAction.cs
@@ -5,20 +5,6 @@ namespace Fumbbl.UI.LogText
 {
     public class PlayerAction : LogTextGenerator<Ffb.Dto.Reports.PlayerAction>
     {
-        private static readonly Dictionary<string, string> ActionStrings = new Dictionary<string, string>()
-        {
-            ["move"] = "starts a Move Action",
-            ["block"] = "starts a Block Action",
-            ["blitzMove"] = "starts a Blitz Action",
-            ["handOverMove"] = "starts a Hand Over Action",
-            ["passMove"] = "starts a Pass Action",
-            ["foulMove"] = "starts a Foul Action",
-            ["standUp"] = "stands up",
-            ["multipleBlock"] = "starts a Block Action",
-            ["standUpBlitz"] = "stands up with Blitz",
-            ["throwBomb"] = "starts a Bomb Action",
-        };
-
         public override IEnumerable<LogRecord> Convert(Ffb.Dto.Reports.PlayerAction report)
         {
             var action = report.playerAction.AsPlayerAction();
