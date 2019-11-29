df562b16f78ea06920d124d0c077b98f9f63d0eb
Christer Kaivo-oja <christer@fumbbl.com>
2019-11-29 22:53:30 +0100

    Add crude player state handling and rendering.

--- Assets/Scripts/Model/Types/PlayerState.cs
+++ Assets/Scripts/Model/Types/PlayerState.cs
@@ -10,7 +10,14 @@ namespace Fumbbl.Model.Types
         public bool IsUnknown => (State & 0xff) == 0x0000;
         public bool IsStanding => (State & 0xff) == 0x0001;
         public bool IsMoving => (State & 0xff) == 0x0002;
+        public bool IsProne => (State & 0xff) == 0x0003;
+        public bool IsStunned => (State & 0xff) == 0x0004;
+        public bool IsKnockedOut => (State & 0xff) == 0x0005;
+
+        public bool IsBeingDragged => (State & 0xff) == 0x000f;
         public bool IsReserve => (State & 0xff) == 0x0009;
+
+        public bool IsActive => (State & 0x0100) != 0;
 
         private PlayerState(int state)
         {
