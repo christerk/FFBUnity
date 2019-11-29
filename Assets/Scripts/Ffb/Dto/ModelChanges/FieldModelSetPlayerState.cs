4247de063ea1192b023a23fde66515e0264a8a0f
Christer Kaivo-oja <christer@fumbbl.com>
2019-11-30 00:17:28 +0100

    Add LogText.GoForItRoll and add handler for SetPlayerState.

---
+++ Assets/Scripts/Ffb/Dto/ModelChanges/FieldModelSetPlayerState.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Fumbbl.Ffb.Dto.ModelChanges
+{
+    class FieldModelSetPlayerState : ModelChange
+    {
+        public string modelChangeId;
+        public string modelChangeKey;
+        public int? modelChangeValue;
+
+        public FieldModelSetPlayerState() : base("fieldModelSetPlayerState")
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Model/ModelChange/FieldModelSetPlayerState.cs
@@ -0,0 +1,19 @@
+﻿using Fumbbl.Model.Types;
+using UnityEngine;
+
+namespace Fumbbl.Model.ModelChange
+{
+    public class FieldModelSetPlayerState : ModelUpdater
+    {
+        public FieldModelSetPlayerState() : base(typeof(Ffb.Dto.ModelChanges.FieldModelSetPlayerState)) { }
+
+        public override void Apply(Ffb.Dto.ModelChange modelChange)
+        {
+            var change = (Ffb.Dto.ModelChanges.FieldModelSetPlayerState)modelChange;
+
+            Player p = FFB.Instance.Model.GetPlayer(change.modelChangeKey);
+            PlayerState state = PlayerState.Get(change.modelChangeValue);
+            p.PlayerState = state;
+        }
+    }
+}
