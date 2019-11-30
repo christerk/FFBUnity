976a15882bed557c927e682bec6fea03ff9db32d
Christer Kaivo-oja <christer@fumbbl.com>
2019-11-30 21:38:00 +0100

    Adding Track Numbers.

---
+++ Assets/Scripts/Ffb/Dto/ModelChanges/TrackNumber.cs
@@ -0,0 +1,8 @@
+﻿namespace Fumbbl.Ffb.Dto.ModelChanges
+{
+    public class TrackNumber
+    {
+        public int number;
+        public int[] coordinate;
+    }
+}
---
+++ Assets/Scripts/View/TrackNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fumbbl.Ffb.Dto.ModelChanges;
+using TMPro;
+
+namespace Fumbbl.View
+{
+    public class TrackNumber : ViewObject<TrackNumber>
+    {
+        public int[] Coordinate;
+        public int Number;
+
+        public override object Key => Coordinate[0] * 100 + Coordinate[1];
+
+        public TextMeshProUGUI LabelObject { get; internal set; }
+
+        public TrackNumber(Ffb.Dto.ModelChanges.TrackNumber square)
+        {
+            Coordinate = square.coordinate;
+            Number = square.number;
+        }
+
+        public override void Refresh(TrackNumber square)
+        {
+            Coordinate = square.Coordinate;
+            Number = square.Number;
+        }
+    }
+}
