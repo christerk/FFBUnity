89f2f13c765b87ef5af09689d186d98ea466c2f9
Christer Kaivo-oja <christer@fumbbl.com>
2019-12-01 20:08:50 +0100

    Remove erronous animation event.

---
+++ Assets/Scripts/UI/LogText/Leader.cs
@@ -0,0 +1,25 @@
+﻿using Fumbbl.Ffb.Dto;
+using Fumbbl.Model;
+using Fumbbl.Model.Types;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fumbbl.UI.LogText
+{
+    public class Leader : LogTextGenerator<Ffb.Dto.Reports.Leader>
+    {
+        public override IEnumerable<LogRecord> Convert(Ffb.Dto.Reports.Leader report)
+        {
+            Team team = FFB.Instance.Model.GetTeam(report.teamId);
+
+            if (string.Equals(report.leaderState, "available"))
+            {
+                yield return new LogRecord($"{team.FormattedName} fain a Leader re-roll.", 1);
+            }
+            else
+            {
+                yield return new LogRecord($"Leader re-roll removed from {team.FormattedName}.", 1);
+            }
+        }
+    }
+}
