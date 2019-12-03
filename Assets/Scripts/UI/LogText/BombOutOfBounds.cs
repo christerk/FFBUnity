785c062f1fc5cdb347fc1aeeddd4d1914bb84520
Szieberth Ádám <sziebadam@gmail.com>
2019-12-03 02:47:40 +0100

    Log Reports: BloodLustRoll, BombOutOfBounds, BribesRoll, CatchRoll (incomplete)

---
+++ Assets/Scripts/UI/LogText/BombOutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using Fumbbl.Model.Types;
+using System.Collections.Generic;
+
+namespace Fumbbl.UI.LogText
+{
+    public class BombOutOfBounds : LogTextGenerator<Ffb.Dto.Reports.BombOutOfBounds>
+    {
+        public override IEnumerable<LogRecord> Convert(Ffb.Dto.Reports.BombOutOfBounds report)
+        {
+            yield return new LogRecord($"<b>Bomb scattered out of bounds.</b>");
+        }
+    }
+}
