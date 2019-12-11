b30e38a01f5dc4b678cbf1a7df1dada273c9315a
SzieberthAdam <sziebadam@gmail.com>
2019-12-11 01:25:48 +0100

    LogText.ScatterBall

---
+++ Assets/Scripts/UI/LogText/ScatterBallRoll.cs
@@ -0,0 +1,21 @@
+﻿using Fumbbl.Model;
+using Fumbbl.Model.Types;
+using Fumbbl.Model.RollModifier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fumbbl.UI.LogText
+{
+    public class ScatterBallRoll : LogTextGenerator<Ffb.Dto.Reports.ScatterBall>
+    {
+        public override IEnumerable<LogRecord> Convert(Ffb.Dto.Reports.ScatterBall report)
+        {
+            string reportstr = report.rolls.Length == 1 ? "Scatter Roll [ " : "Scatter Rolls [ ";
+            reportstr += string.Join(", ", report.rolls.Select(x=>x.ToString()).ToArray());
+            reportstr += " ] ";
+            reportstr += string.Join(", ", report.directionArray);
+            yield return new LogRecord($"<b>{reportstr}</b>");
+        }
+    }
+}
