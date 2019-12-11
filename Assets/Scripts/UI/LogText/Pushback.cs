415e3d8f9d47383d76101bf30aeeedc8d9deb697
SzieberthAdam <sziebadam@gmail.com>
2019-12-11 01:12:35 +0100

    LogText.Pushback

---
+++ Assets/Scripts/UI/LogText/Pushback.cs
@@ -0,0 +1,29 @@
+﻿using Fumbbl.Model;
+using Fumbbl.Model.Types;
+using Fumbbl.Model.RollModifier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fumbbl.UI.LogText
+{
+    public class Pushback : LogTextGenerator<Ffb.Dto.Reports.Pushback>
+    {
+        public override IEnumerable<LogRecord> Convert(Ffb.Dto.Reports.Pushback report)
+        {
+            Player attacker = FFB.Instance.Model.GetPlayer(FFB.Instance.Model.ActingPlayer.PlayerId);
+            Player defender = FFB.Instance.Model.GetPlayer(report.defenderId);
+            switch (report.pushbackMode)
+            {
+                case "regular":
+                    break;
+                case "grab":
+                    yield return new LogRecord($"{attacker.FormattedName} uses Grab to place {attacker.Gender.Genetive} opponent.", 1);
+                    break;
+                case "sideStep":
+                    yield return new LogRecord($"{defender.FormattedName} uses Side Step to avoid being pushed.", 1);
+                    break;
+            }
+        }
+    }
+}
