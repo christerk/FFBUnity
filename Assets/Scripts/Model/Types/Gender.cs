6e63d636d465a4dc180edb5fb9e5cfa57a413cd7
SzieberthAdam <sziebadam@gmail.com>
2019-12-13 18:46:39 +0100

    Zero change order of the Dative, Genetive, and Nominative fields

--- Assets/Scripts/Model/Types/Gender.cs
+++ Assets/Scripts/Model/Types/Gender.cs
@@ -2,14 +2,14 @@
 {
     public class Gender
     {
-        public string Genetive { get; private set; }
         public string Dative { get; private set; }
+        public string Genetive { get; private set; }
         public object Nominative { get; private set; }
 
         public static Gender Male = new Gender()
         {
-            Genetive = "his",
             Dative = "him",
+            Genetive = "his",
             Nominative = "he"
         };
 
