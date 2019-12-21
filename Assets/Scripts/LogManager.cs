0ecf5b3a9332c7e17276084dfe0aa5f6710ccee7
stowelly <michael.stowell84@gmail.com>
2019-12-21 17:34:36 +0000

    Creating LogManager

---
+++ Assets/Scripts/LogManager.cs
@@ -0,0 +1,21 @@
+﻿using Fumbbl;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fumbbl
+{
+    enum LogLevel
+    {
+        OFF,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        ALL
+    }
+    class LogManager
+    {
+
+
+    }
+}
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,41 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+namespace Fumbbl
 {
-    public AudioSource SoundEffectSource;
-
-    private Dictionary<string, AudioClip> SoundEffectClips;
-
-    #region MonoBehaviour Methods
-
-    private void Start()
+    public class SoundManager : MonoBehaviour
     {
-        FFB.Instance.OnSound += Play;
-        Object[] AudioFiles;
-        AudioFiles = Resources.LoadAll("Audio", typeof(AudioClip));
+        public AudioSource SoundEffectSource;
 
-        SoundEffectClips = new Dictionary<string, AudioClip>();
+        private Dictionary<string, AudioClip> SoundEffectClips;
 
-        foreach(AudioClip file in AudioFiles)
+        #region MonoBehaviour Methods
+
+        private void Start()
         {
-            SoundEffectClips.Add(file.name, file);
+            FFB.Instance.OnSound += Play;
+            Object[] AudioFiles;
+            AudioFiles = Resources.LoadAll("Audio", typeof(AudioClip));
+
+            SoundEffectClips = new Dictionary<string, AudioClip>();
+
+            foreach (AudioClip file in AudioFiles)
+            {
+                SoundEffectClips.Add(file.name, file);
+            }
         }
-    }
 
-    private void OnDisable()
-    {
-        FFB.Instance.OnSound -= Play;
-    }
-
-    #endregion
-
-    public void Play(string sound)
-    {
-        AudioClip clip = SoundEffectClips[sound];
-        if(clip != null && !FFB.Instance.Settings.Sound.Mute)
+        private void OnDisable()
         {
-            SoundEffectSource.PlayOneShot(clip, FFB.Instance.Settings.Sound.GlobalVolume);
+            FFB.Instance.OnSound -= Play;
+        }
+
+        #endregion
+
+        public void Play(string sound)
+        {
+            AudioClip clip = SoundEffectClips[sound];
+            if (clip != null && !FFB.Instance.Settings.Sound.Mute)
+            {
+                SoundEffectSource.PlayOneShot(clip, FFB.Instance.Settings.Sound.GlobalVolume);
+            }
         }
     }
 }
