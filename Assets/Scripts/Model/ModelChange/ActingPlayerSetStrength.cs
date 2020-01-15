9887cee375628155bc962342ef2a6125cde6e62b
Michael Stowell <mike@MICHAEL_STOWELL.ca.SEGA.Internal>
2020-01-15 13:19:52 +0000

    Implemented the most annoyingly spammy model changes, should clear the way for future important ones to not be ignored

---
+++ Assets/Scripts/Model/ModelChange/ActingPlayerSetStrength.cs
@@ -0,0 +1,12 @@
+﻿namespace Fumbbl.Model.ModelChange
+{
+    public class ActingPlayerSetStrength : ModelUpdater<Ffb.Dto.ModelChanges.ActingPlayerSetStrength>
+    {
+        public ActingPlayerSetStrength() : base(typeof(Ffb.Dto.ModelChanges.ActingPlayerSetStrength)) { }
+
+        public override void Apply(Ffb.Dto.ModelChanges.ActingPlayerSetStrength change)
+        {
+            FFB.Instance.Model.ActingPlayer.Strength = change.modelChangeValue;
+        }
+    }
+}
